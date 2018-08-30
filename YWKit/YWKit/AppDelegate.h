e5b1f57a21565b0bd7a5a76e9a0f566c863b6538
yanwei <654811239@qq.com>
2018-08-30 14:58:52 +0800

    first commit

---
+++ YWKit/YWKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YWKit
+//
+//  Created by yanwei on 2018/8/30.
+//  Copyright © 2018年 闫威威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YWKit/YWKit/Source/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YWKit
+//
+//  Created by yanwei on 2018/8/30.
+//  Copyright © 2018年 闫威威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
